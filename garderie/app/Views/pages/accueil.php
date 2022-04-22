c9a116de8c631bc51d0c6206c6b67c83cdc3bf97
ahmedooo1 <aahmadooo997@gmail.com>
2022-04-22 11:52:25 +0200

    update accueil

--- garderie/app/Views/pages/accueil.php
+++ garderie/app/Views/pages/accueil.php
@@ -1,4 +1,3 @@
-
 <?= $this->extend('master') ?>
 
 <?= $this->section('css') ?>
@@ -22,19 +21,83 @@
 
         </div>
         <div class="sec1-p">
-            <p>une plateforme hyper complète et très fonctionnelle qui permet aux parents <br> de géolocaliser la baby-sitter la plus proche de leur domicile.</p>
+            <p>une plateforme hyper complète et très fonctionnelle qui permet aux parents de géolocaliser la baby-sitter la plus proche de leur domicile.</p>
         </div>
     </div>
 </section>
 <section class="sec2">
-
+    <title></title>
     <div>
         <img src="<?= base_url()  ?>/img/sec2.png" alt='sec2'>
     </div>
     <div>
-        <p>Découvrez notre site et tout nos services dans le site <br>Une équipe à votre disposition 7/7</p>
+        <p>Découvrez notre site et nos services. <br>Une équipe à votre disposition 7/7</p>
         <button>Contactez Nous</button>
     </div>
+</section>
+
+<section class="sec3">
+    <div class="container">
+        <br>
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+                <li data-target="#myCarousel" data-slide-to="3"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+
+                <div class="item active">
+                    <img src="<?= base_url()  ?>/img/bienvenue.jpg" alt="bv" width="460" height="345">
+                    <div class="carousel-caption">
+                        <h3>Bienvenue</h3>
+                        <p>Dans votre site Baby Garde.</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="<?= base_url()  ?>/img/inscrivez.jpg" alt=" inscrivez vous.." width="460" height="345">
+                    <div class="carousel-caption">
+                        <h3>Inscrivez-vous dès maintenant</h3>
+                        <p>Rejoignez nous en tant que professionelle ou Particulier.</p>
+                    </div>
+                </div>
+
+                <div class="item">
+                    <img src="<?= base_url()  ?>/img/sc.png" alt="Flower" width="460" height="345">
+                    <div class="carousel-caption">
+                        <h3>Service client 7/7</h3>
+                        <p>Nous sommes toujours à votre disposition si besoin.</p>
+                    </div>
+                </div>
+
+                <div class="item">
+
+                    <img src="<?= base_url()  ?>/img/confi.jpg" alt=" conf" width="460" height="345">
+                    <div class="carousel-caption">
+                        <h3>Vos enfants sont entre de bonnes mains.</h3>
+                        <p>N'hésitez pas à choisir un sitter pour vos enfants en toute sérénité ?</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+
 </section>
 <?= $this->endSection() ?>
 
--- garderie/app/Views/template/footer.php
+++ garderie/app/Views/template/footer.php
@@ -1,27 +1,31 @@
 <footer>
-    <ul class="footer_case" id="pages">
-        <li class="titre_footer ">PAGES</li>
-        <li><a href="">Accueil</a></li>
-        <li><a href="">Qui sommes-nous</a></li>
-        <li><a href="">Se connecter</a></li>
-        <li><a href="">Mentions legales</a></li>
-        <li><a href="">Conditions générales d'utilisations</a></li>
-    </ul>
-
-    <ul class="footer_case" id="logo_infos">
-        <img src="<?= base_url()  ?>/img/garderie.png" alt="logo" width="300px">
-    </ul>
-
-    <ul class="footer_case" id="informations">
-        <li class="titre_footer ">INFORMATIONS</li>
-        <li> 02.77.98.54.21</li>
-        <li> Baby-Garde@contact.com</li>
-        <ul class="footer_case" id="reseaux_sociaux">
-            <a href=""><img src="<?= base_url()  ?>/img/facebook.png"></a>
-            <a href=""><img src="<?= base_url()  ?>/img/twitter.png"></a>
-            <a href=""><img src="<?= base_url()  ?>/img/instagram.png"></a>
-            <a href=""><img src="<?= base_url()  ?>/img/linkedin.png"></a>
+    <div class="container-F">
+        <ul class="footer_case" id="pages">
+            <h3 class="titre_footer ">PAGES</h3>
+            <li><a href="">Accueil</a></li>
+            <li><a href="">Qui sommes-nous</a></li>
+            <li><a href="">Se connecter</a></li>
+            <li><a href="">Mentions legales</a></li>
+            <li><a href="">Conditions générales d'utilisations</a></li>
         </ul>
-    </ul>
 
+        <ul class="footer_case" id="logo_infos">
+            <img src="<?= base_url()  ?>/img/garderie.png" alt="logo" width="300px">
+        </ul>
+
+        <ul class="footer_case" id="infos">
+            <h3 class="titre_footer ">INFORMATIONS</h3>
+            <li> 02.77.98.54.21</li>
+            <li> Baby-Garde@contact.com</li>
+
+        </ul>
+
+    </div>
+    <hr>
+    <ul class="footer_case" id="reseaux_sociaux">
+        <a href=""><img src="<?= base_url()  ?>/img/facebook.png"></a>
+        <a href=""><img src="<?= base_url()  ?>/img/twitter.png"></a>
+        <a href=""><img src="<?= base_url()  ?>/img/instagram.png"></a>
+        <a href=""><img src="<?= base_url()  ?>/img/linkedin.png"></a>
+    </ul>
 </footer>
