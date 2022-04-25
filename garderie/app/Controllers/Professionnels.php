98745b3e869d25afc89f08792c333bbfd2da8428
Fabsend <97021007+Fabsend@users.noreply.github.com>
2022-04-25 16:56:10 +0200

    update creation annonce

--- garderie/app/Controllers/Professionnels.php
+++ garderie/app/Controllers/Professionnels.php
@@ -143,7 +143,9 @@ class Professionnels extends BaseController
                 $professionnels = $this->generateNewsDataFromPost();
                 $this->ProfessionnelsModel->insert($professionnels);
             }
-            return view('professionnels/create');
+            return redirect()->to('/');
+        } else {
+            echo view("professionnels/create");
         }
     }
     function profil()
--- garderie/app/Views/professionnels/create.php
+++ garderie/app/Views/professionnels/create.php
@@ -7,19 +7,49 @@
     <title>create</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="<?= base_url(); ?>/css/style.css" rel="stylesheet">
+    <link href="<?= base_url(); ?>/css/stylePros.css" rel="stylesheet">
     <script type="text/javascript" src="<?= base_url(); ?>/js/main.js"></script>
 </head>
 
 <body>
-    <div class="input-field col s12">
-        <label for="email" class="active" style="color: black;">titre</label>
-        <input class="validate" type="text" value="" name="titre" placeholder="creche bois-guillaume" required="">
-    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m12 l12" style="text-align:center; color:white;"><a href="<?= base_url(); ?>/">
+                    <img src="<?= base_url();  ?>/img/garderie.png" width="250px" alt=""></a>
+                <h3>crée ton Annonce</h3>
+                <p>Bienvenue sur la page de Création d'annonce!</p>
+            </div>
+        </div>
+        <div class="row" id="blanc">
+            <div class="col l12 m12 s12" id="rectangle">
+                <form method="post">
+                    <div class="input-field col s6">
+                        <label for="titre" class="active" style="color: black;">titre</label>
+                        <input type="text" value="" name="titre" placeholder="ex: Creche bois-guillaume" required="">
+                    </div>
+                    <div class="input-field col s6">
+                        <label for="debut_session" class="active" style="color: black;">horaire de debut</label>
+                        <input type="text" value="" name="debut_session" placeholder="ex: 7h" required="">
+                    </div>
+                    <div class="input-field col s6">
+                        <label for="fin_session" class="active" style="color: black;">horaire de fin</label>
+                        <input type="text" value="" name="fin_session" placeholder="ex: 19h" required="">
+                    </div>
 
-    <div class="input-field col s12">
-        <label for="naissance" class="active" style="color: black;">Date de debut</label>
-        <input type="text" class="datepicker" name="debut" placeholder="10h-11h" required="">
+                    <div class="input-field col s6">
+                        <label for="email" class="active" style="color: black;">nombres de places</label>
+                        <input class="validate" type="text" value="" name="nbr_place_restant" placeholder="ex: Dupond1990@gmail.com" required="">
+                    </div>
+
+
+                    <div class="input-field col s12">
+                        <button class="btn waves-effect waves-light pulse" type="submit" name="action">Valider
+                            <i class="material-icons right">send</i>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 
 </body>
