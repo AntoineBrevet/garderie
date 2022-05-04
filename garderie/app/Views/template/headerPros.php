51c299fa4331f04750eca8aed27229132ff55983
AntoineBrevet <antoine.brevet@live.fr>
2022-05-04 14:01:36 +0200

    commit date format / redirect

--- garderie/app/Views/template/headerPros.php
+++ garderie/app/Views/template/headerPros.php
@@ -2,20 +2,26 @@
 
     <input id="nav-toggle" type="checkbox">
     <div class="logo">
-
-        <a href=""><img src="<?= base_url()  ?>/img/garderie.png" alt="logo"></a>
+        <?php
+        if (session("professionnels")) { ?>
+            <a href="<?= base_url()  ?>/prosIndex"><img src="<?= base_url()  ?>/img/garderie.png" alt="logo"></a>
+        <?php
+        } else { ?>
+            <a href="<?= base_url()  ?>/professionnels"><img src="<?= base_url()  ?>/img/garderie.png" alt="logo"></a>
+        <?php
+        }
+        ?>
     </div>
 
     <ul class="links">
         <li><a href="<?= base_url(); ?>/">Mode utilisateurs</a></li>
         <?php
-        if (session("professionnels")) {?>
+        if (session("professionnels")) { ?>
             <li><a href="<?= base_url()  ?>/create"><img src="<?= base_url()  ?>/img/create.png" alt="">Créer une annonce</a></li>
             <li><a href="<?= base_url()  ?>/profilPros"><img src="<?= base_url()  ?>/img/profile.png" alt="">Profile</a></li>
             <li class="deconnexion"><img src="<?= base_url()  ?>/img/logout.png" alt=""><a href="<?= base_url() ?>/deconnexionPros">Déconnexion</a></li>
         <?php
-        }
-        else{?>
+        } else { ?>
             <li><a href="<?= base_url(); ?>/inscriptionPros">Inscription</a></li>
             <li><a href="<?= base_url(); ?>/connexionPros">Connexion</a></li>
         <?php
