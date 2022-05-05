86c163c8bb7a722d08466e2ce73132ba6d9956e0
ahmedooo1 <aahmadooo997@gmail.com>
2022-05-05 17:13:34 +0200

    update style userIndex

--- garderie/app/Views/utilisateurs/utilisateursIndex.php
+++ garderie/app/Views/utilisateurs/utilisateursIndex.php
@@ -52,13 +52,18 @@ include 'messagesPvPros.php';
     </div>
 
 </section>
-<label>debut</label>
-<input type="date" name="dateDebut" id="dateDebut" style="color:black"><br>
-<label>fin</label>
+<div class="dates">
+    <div>
+        <label>Debut : </label>
+        <input type="date" name="dateDebut" id="dateDebut" style="color:black">
+    </div>
+    <br>
+    <div>
+        <label>Fin : </label>
 
-<input type="date" name="dateFin" id="dateFin" style="color:black">
-
-
+        <input type="date" name="dateFin" id="dateFin" style="color:black">
+    </div>
+</div>
 <div class="feed">
 
     <?php
@@ -402,7 +407,6 @@ include 'messagesPvPros.php';
     }
 
     var urlAjaxMessage = "<?= base_url('utilisateursIndex'); ?>"
-
 </script>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
