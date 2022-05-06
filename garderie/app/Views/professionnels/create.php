207a1f573fb96b2d14cf5d049d226c91cb58fdab
selim27000 <selim27000@hotmail.com>
2022-05-06 13:36:41 +0200

    final

--- garderie/app/Views/professionnels/create.php
+++ garderie/app/Views/professionnels/create.php
@@ -47,7 +47,6 @@
                         <label for="prix" class="active" style="color: black;">Prix en € (par heure)</label>
                         <input type="number" value="" name="prix" placeholder="ex: 12" required="">
                     </div>
-
                     <div class="input-field col s12">
                         <button class="btn waves-effect waves-light pulse" type="submit" name="action">Valider
                             <i class="material-icons right">send</i>
